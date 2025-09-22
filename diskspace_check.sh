28fb5385a565e28ef65fe0530d261a81989f5b38
aruenjali <aruenjali@gmail.com>
2025-09-22 21:21:19 +0530

    practice

--- diskspace_check.sh
+++ diskspace_check.sh
@@ -5,7 +5,9 @@ ROOT=$(df -h / | awk 'NR==2 {print $5}' |tr -d %)
 BOOT=$(df -h /boot | awk 'NR==2 {print $5}' |tr -d %)
 if [ $ROOT -gt 25 ]; then
     echo "Root mount point is reached to maximum size $ROOT"
-else [ $BOOT -gt 60 ]; then
-    echo "Boot mount point is reached to maximum size $ROOT and it is Critical"
+elif [ $BOOT -gt 60 ]; then
+    echo "Boot mount point is reached to maximum size $BOOT and it is Critical"
+else
+    echo "All mount point $ROOT & $BOOT is under control"
 fi
     
