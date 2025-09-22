fa8998d32baadff31a3f0d6ed7a408d38ec54b6f
aruenjali <aruenjali@gmail.com>
2025-09-22 20:00:13 +0530

    practice

--- file_permition.sh
+++ file_permition.sh
@@ -3,7 +3,7 @@ DIR=/root/scripts/arun_shell_scripts
 echo "enter the filename:"
 read filename
 
-if [-e "$DIR/$filename"]; then
+if [ -e "$DIR/$filename" ]; then
     echo "The given file is $filename is exists"
     ls -ltr "$DIR/$filename"
 else
