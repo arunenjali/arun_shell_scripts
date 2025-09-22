7aa943dd1dce7273dd5aee60461a52bf58b2328e
aruenjali <aruenjali@gmail.com>
2025-09-22 20:42:51 +0530

    practice

--- number_devide.sh
+++ number_devide.sh
@@ -2,7 +2,7 @@
 echo "enter the number:"
 read number
 
-if [ ( number % 5 == 0 ) && ( number % 5 == 0 )];then
+if [ (number % 5 == 0) && (number % 5 == 0)];then
     echo "The number is divisible by 5 and 7"
 else
     echo "The number is nt divisible"
