1660668c34c9e6c6f22101ec05397e9fca0c14ed
aruenjali <aruenjali@gmail.com>
2025-09-22 20:33:56 +0530

    practice

--- username_check.sh
+++ username_check.sh
@@ -3,7 +3,7 @@ echo "enter the username:"
 read username
 
 if grep -q "^$username:" /etc/passwd;then
-    echo " please find the below are the username,directory and bash for the user $username"
+    echo " please find the below are the $username home directory and bash for the user"
     grep "^$username:" /etc/passwd | cut -d: -f1,6,7
 else
     echo "the username $USER is not exists"
