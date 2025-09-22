d05f6ff09e826f3745fc8c9e71b01dca819eafb4
aruenjali <aruenjali@gmail.com>
2025-09-22 19:28:29 +0530

    practice

--- file_check.sh
+++ file_check.sh
@@ -2,7 +2,7 @@
 echo "Enter the file name to check:"
 read filename
 
-if [-f $filename]; then
+if [-f "$filename"]; then
     echo "The given filename $filename is exists"
 else
     echo " The given filename $filename is not available"
