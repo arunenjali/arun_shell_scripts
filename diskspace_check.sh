4237687eaa1d5686bbe0d79096d29bd2cf54ef1b
aruenjali <aruenjali@gmail.com>
2025-09-22 21:12:47 +0530

    practice

--- diskspace_check.sh
+++ diskspace_check.sh
@@ -6,5 +6,5 @@ DISK=$(df -h / | awk 'NR==2 {print $5}' |tr -d %)
 if ( $DISK -gt 25 ); then
     echo "/ mount point is reached to maximum size"
 else 
-    echo "it is under control
+    echo "it is under control"
 fi
