e5bc0aa2e4b4f0e5f03c5c6785d9a8abcc6b4d55
aruenjali <aruenjali@gmail.com>
2025-09-22 20:26:47 +0530

    practice

--- username_check.sh
+++ username_check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "enter the username:"
 read username
-USER=(cut -d: -f1 cat /etc/passwd)
+USER=$(cut -d: -f1 cat /etc/passwd)
 
-if [ $username -eq $USER ];then
+if [ $username = $USER ];then
     echo " please find the below are the username,directory and bash for the user $username"
-    cut -d: -f1,6,7 cat /etc/passwd
+    cut -d: -f1,6,7 /etc/passwd
 else
     echo "the username $USER is not exists"
 fi
