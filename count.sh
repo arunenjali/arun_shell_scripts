6ac74733a06fca852c3d373befdf217da72cab0d
aruenjali <aruenjali@gmail.com>
2025-09-26 13:55:21 +0530

    practice

--- count.sh
+++ count.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-WORDS=cat sample.txt |wc -w
-LINES=cat sample.txt |wc -l
-CHARACTERS=cat sample.txt |wc -m
+WORDS=$(cat sample.txt |wc -w)
+LINES=$(cat sample.txt |wc -l)
+CHARACTERS=$(cat sample.txt |wc -m)
 
 echo "Total no of words in the file is $WORDS"
 echo "Total no of lines in the file is $LINES"
