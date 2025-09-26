1dab625a703d24c17325ca47888e4d648004e1d9
aruenjali <aruenjali@gmail.com>
2025-09-26 13:52:46 +0530

    practice

--- count.sh
+++ count.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 WORDS=cat sample.txt |wc -w
-LINES=cat sample.txt | wc -l
+LINES=cat sample.txt |wc -l
 CHARACTERS=cat sample.txt |wc -m
 
-echo "Total no of words in the file is $WORDS "
-echo "Total no of lines in the file is $LINES "
-echo "Total no of characters in the file is $CHARACTERS "
+echo "Total no of words in the file is $WORDS"
+echo "Total no of lines in the file is $LINES"
+echo "Total no of characters in the file is $CHARACTERS"
 
